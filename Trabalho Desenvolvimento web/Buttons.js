7bc5b12a84d473fd6f83c5768a47a4f9d02592e1
LucaRenoldi <94394945+LucaRenoldi@users.noreply.github.com>
2021-11-16 19:06:41 -0300

    Add files via upload

---
+++ Trabalho Desenvolvimento web/Buttons.js
@@ -0,0 +1,16 @@
+function fraseFormulario(){
+    var paragrafo = document.getElementById("submit");
+    paragrafo.innerHTML = "Se você conhece o inimigo e conhece a si mesmo, não precisa temer o resultado de cem batalhas. Se você se conhece mas não conhece o inimigo, para cada vitória ganha sofrerá também uma derrota. Se você não conhece nem o inimigo nem a si mesmo, perderá todas as batalhas...";
+}
+
+
+function fraseSupport(){
+    var paragrafo = document.getElementById("support");
+    paragrafo.innerHTML = "We will try our best to solve your problem as quick as possible!";
+}
+
+
+function fraseSuggestions(){
+    var paragrafo = document.getElementById("suggestions");
+    paragrafo.innerHTML = "Thank you!";
+}
